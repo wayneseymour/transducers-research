547ea3cda729a1406e2df3344da9b51151abee98
Tre' Seymour <wayne.seymour@elastic.co>
2020-04-14 23:29:15 -0600

    Lil cleaner, imho

--- __tests__/james-sinclair-transducers/index.spec.js
+++ __tests__/james-sinclair-transducers/index.spec.js
@@ -1,5 +1,7 @@
 
-import {victorianSlang, makeFilterTransducer, makeMapTransducer, addScores, getPopularity, isFound} from '../../james-sinclair-transducers';
+import {makeFilterTransducer, makeMapTransducer, addScores, getPopularity, isFound} from '../../james-sinclair-transducers';
+
+
 
 describe(`james-sinclair-transducers`, () => {
 
@@ -9,8 +11,7 @@ describe(`james-sinclair-transducers`, () => {
   const allInOneReducer = foundFilterTransducer(scoreMappingTransducer(addScores));
 
   const initialInfo = {totalPopularity: 0, itemCount: 0};
-  const popularityInfo = victorianSlang.reduce(allInOneReducer, initialInfo);
-
+  const popularityInfo = victorianSlang().reduce(allInOneReducer, initialInfo);
 
   it(`should transduce down to 400.5`, () => {
     const {totalPopularity, itemCount} = popularityInfo;
@@ -18,3 +19,53 @@ describe(`james-sinclair-transducers`, () => {
     expect(400.5).toEqual(averagePopularity);
   });
 });
+
+function victorianSlang() {
+ return [
+   {
+     term: 'doing the bear',
+     found: true,
+     popularity: 108,
+   },
+   {
+     term: 'katterzem',
+     found: false,
+     popularity: null,
+   },
+   {
+     term: 'bone shaker',
+     found: true,
+     popularity: 609,
+   },
+   {
+     term: 'smothering a parrot',
+     found: false,
+     popularity: null,
+   },
+   {
+     term: 'damfino',
+     found: true,
+     popularity: 232,
+   },
+   {
+     term: 'rain napper',
+     found: false,
+     popularity: null,
+   },
+   {
+     term: 'donkey’s breakfast',
+     found: true,
+     popularity: 787,
+   },
+   {
+     term: 'rational costume',
+     found: true,
+     popularity: 513,
+   },
+   {
+     term: 'mind the grease',
+     found: true,
+     popularity: 154,
+   },
+ ]
+};
