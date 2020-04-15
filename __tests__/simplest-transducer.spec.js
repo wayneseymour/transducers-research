e465b8fa4e0674f3286b34449fae22a0c9475ac6
Tre' Seymour <wayne.seymour@elastic.co>
2020-04-15 10:58:30 -0600

    Typo

--- __tests__/simplest-transducer.spec.js
+++ __tests__/simplest-transducer.spec.js
@@ -3,7 +3,7 @@ import {identityTransducerTransformer, iterator, transduce} from '../simplest-tr
 describe(`simplest transducer`, () => {
   describe(`using an identity fn as transducer / transformer`, () => {
     describe(`and using [].concat() as a mapper / iterator fn `, () => {
-      it(`should xform down to an array with the idential contents`, () => {
+      it(`should xform down to an array with the identical contents`, () => {
         const initialValue = [];
         const arr = [0, 1, 2, 3, 4]
         const xs = transduce(identityTransducerTransformer)(iterator)(initialValue, arr); // [1,2,3,4,5]
